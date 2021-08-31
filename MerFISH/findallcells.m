353f51942db134a5040645e90af565fdc34d3a32
lbinan <79212104+lbinan@users.noreply.github.com>
2021-08-31 15:16:31 -0400

    Add files via upload

---
+++ MerFISH/findallcells.m
@@ -0,0 +1,35 @@
+function cellmask=findallcells(mypath,genelist)
+
+this=imfinfo(fullfile(mypath,'GenerateMosaic','images','mosaic_DAPI_0.tif'));
+zeroImage=zeros(this.Height, this.Width);
+myImage=zeros(size(zeroImage));
+load(fullfile(mypath,'positions2keep.mat'));
+
+
+for i=1:size(genelist,2)
+    for k=1:7
+        myImage=myImage+generateImage(mypath,genelist(1,i),k);
+    end
+end
+microglias=findcells(mypath);
+myImage=myImage.*(ones(size(myImage))-microglias);
+myCroppedImage=zeroImage;
+myCroppedImage(keeppositions)=myImage(keeppositions);
+myImage=imbinarize(myImage);
+SE=strel('disk',3);
+thisImage=imdilate(myImage,SE);
+SE=strel('disk',3);
+thisImage=imdilate(thisImage,SE);
+SE=strel('disk',2);
+thisImage=imerode(thisImage,SE);
+SE=strel('disk',2);
+thisImage=bwareaopen(imdilate(thisImage,SE),600);
+thisImage=thisImage-bwareaopen(imdilate(thisImage,SE),1500);
+SE=strel('disk',5);
+thisImage=bwareaopen(imdilate(thisImage,SE),800);
+thisImage=thisImage-bwareaopen(imdilate(thisImage,SE),3000);
+SE=strel('disk',6);
+thisImage=imfill(bwareaopen(thisImage,800),'holes');
+cellmask=thisImage;
+% rgb=cat(3,zeros(size(thisImage)),255*myImage,255*(imdilate(thisImage,SE)-thisImage));
+% figure, imshow(rgb);
